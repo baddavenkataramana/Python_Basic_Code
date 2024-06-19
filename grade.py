a0c193ff76a5ff12316e55cf94e6fcb69ccf07d1
baddavenkataramana <146856626+baddavenkataramana@users.noreply.github.com>
2024-06-19 15:42:08 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,10 @@
+marks=int(input("enter the marks:"))
+if(marks>=90):
+    print("A GRADE")
+
+elif(marks>=75 and marks<90):
+    print("B GRADE")
+elif(marks>=45 and marks<75):
+    print("C GRADE")
+else:
+    print("FAIL")
